7554f523c31db06dc0e72bef8b4c4f2150b1b2be
Super Hackio <superhaxio@gmail.com>
2026-05-21 19:16:23 -0700

    Update AssignableArray and Vector

--- include/Game/Util/Array.h
+++ include/Game/Util/Array.h
@@ -3,118 +3,248 @@
 #include "revolution.h"
 
 namespace MR {
-    template<class T>
-    class Vector;
-
-    template<class T>
+    /// @brief A contiguous reallocatable array.
+    template < class T >
     class AssignableArray {
     public:
-        inline AssignableArray() {
+        typedef T Item;
+
+        /// @brief Creates a new `AssignableArray`.
+        AssignableArray() {
             mArr = 0;
             mMaxSize = 0;
         }
 
-        inline void init(s32 cnt) {
-            mArr = new T[cnt];
-            mMaxSize = cnt;
+        /// @brief Creates a new `AssignableArray`.
+        /// @param num The number of elements.
+        AssignableArray(s32 num) { init(num); }
+
+        /// @brief Destroys the `AssignableArray`.
+        ~AssignableArray() {
+            if (mArr != nullptr) {
+                delete[] mArr;
+            }
         }
+
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        T& operator[](int idx) { return mArr[idx]; }
+
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        const T& operator[](int idx) const { return mArr[idx]; }
+
+        /// @brief Allocates memory for the array.
+        /// @param num The number of elements.
+        void init(s32 num) {
+            mArr = new T[num];
+            mMaxSize = num;
+        }
+
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
+        int size() const { return mMaxSize; }
 
         inline void assign(T what, s32 where) {
             mArr[where] = what;
         }
 
-        inline T* begin() {
-            return mArr;
-        }
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        T* begin() { return &mArr[0]; }
 
-        inline T* end() {
-            return &mArr[mMaxSize];
-        }
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        const T* begin() const { return &mArr[0]; }
 
-        ~AssignableArray() {
-            if (mArr) {
-                delete[] mArr;
-            }
-        }
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        T* end() { return &mArr[mMaxSize]; }
 
-        typedef T Item;
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        const T* end() const { return &mArr[mMaxSize]; }
 
-        T* mArr;        // _0
-        s32 mMaxSize;   // _4
+        /// @brief The pointer to the array of elements.
+        /* 0x0 */ T* mArr;
+
+        /// @brief The number of elements.
+        /* 0x4 */ s32 mMaxSize;
     };
 
-    template<class T, int C>
+    /// @brief A contiguous fixed-size array.
+    template < class T, int N >
     class FixedArray {
     public:
         typedef T Item;
+        template < class X >
+        struct RemovePtr {
+            typedef X Type;
+        };
+        template < class X >
+        struct RemovePtr< X* > {
+            typedef X Type;
+        };
+        typedef RemovePtr< T >::Type Base;
 
-        inline ~FixedArray() {}
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        T& operator[](int idx) { return mArr[idx]; }
 
-        inline int getSize() {
-            return C;
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        const T& operator[](int idx) const { return mArr[idx]; }
+
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
+        int size() const { return N; }
+
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        T* begin() { return &mArr[0]; }
+
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        const T* begin() const { return &mArr[0]; }
+
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        T* end() { return &mArr[N]; }
+
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        const T* end() const { return &mArr[N]; }
+
+        void callAllFunc(void (Base::* func)()) {
+            for (T* it = begin(); it != end(); ++it) {
+                ((*it)->*func)();
+            }
         }
 
-        T mArr[C];          // _0
+    private:
+        /// @brief The array of elements.
+        /* 0x0 */ T mArr[N];
     };
 
-    template<class T>
+    /// @brief A contiguous growable array.
+    template < class T >
     class Vector {
     public:
-        inline Vector() {}
-        inline ~Vector() {}
+        /// @brief Creates a new `Vector`.
+        Vector() { clear(); }
 
-        inline void assign(T::Item &pItem, int where) {
+        /// @brief Destroys the `Vector`.
+        ~Vector() {}
+
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        T::Item& operator[](int idx) { return mArray[idx]; }
+
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
+        const T::Item& operator[](int idx) const { return mArray[idx]; }
+
+        /// @brief Allocates memory for the array.
+        /// @param num The number of elements.
+        void init(s32 num) { mArray.init(num); }
+
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
+        int size() const { return mCount; }
+
+        /// @brief Returns the number of elements that can be stored without reallocating.
+        /// @return The number of elements that can be stored.
+        int capacity() const { return mArray.size(); }
+
+        inline void assign(T::Item& pItem, int where) {
             mArray.mArr[where] = pItem;
         }
 
-        inline void assignToMax(T::Item &pItem) {
+        inline void assignToMax(T::Item& pItem) {
             assign(pItem, mCount);
         }
 
-        // Taken from Petari
-        T::Item* erase (T::Item* pIter) {
+        /// @brief Appends a copy of `rItem` to the end of the container.
+        /// @param rItem The reference to the value to append.
+        void push_back(const T::Item& rItem) {
+            u32 index = mCount;
+
+            mCount++;
+            mArray[index] = rItem;
+        }
+
+        /// @brief Removes the value at the given position from the container.
+        /// @param pIter The pointer to the position where the value should be removed.
+        /// @return The pointer to the position of the removed value.
+        T::Item* erase(T::Item* pIter) {
             if (end() - pIter - 1 > 0) {
                 for (T::Item* p = pIter; p + 1 != end(); p++) {
                     *p = *(p + 1);
                 }
             }
+
             mCount--;
 
             return pIter;
         }
 
-        void push_back(const T::Item &rItem) {
-            u32 count = mCount;
-            mCount++;
-            mArray.mArr[count] = rItem;
-        }
+        /// @brief Removes all values from the container.
+        void clear() { mCount = 0; }
 
-        T::Item* begin() {
-            return mArray.mArr;
-        }
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        T::Item* begin() { return &mArray[0]; }
 
-        T::Item* end() {
-            return &mArray.mArr[mArray.mMaxSize];
-        }
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
+        const T::Item* begin() const { return &mArray[0]; }
 
-        T mArray;   // _0
-        u32 mCount; // _8
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        T::Item* end() { return &mArray[mCount]; }
+
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
+        const T::Item* end() const { return &mArray[mCount]; }
+
+        /// @brief The array of elements.
+        /* 0x0 */ T mArray;
+
+        /// @brief The number of valid elements.
+        /* 0x? */ s32 mCount;
     };
 
-    template<class T, int S>
+    template < class T, int S >
     class FixedRingBuffer {
     public:
         class iterator {
         public:
-            iterator(T *head, T *tail) {
-                mHead = head;
-                mTail = tail;
-                mEnd = head + S;
-            }
+            inline iterator() {}
 
-            T* mHead; // _0
-            T* mTail; // _4
-            T* mEnd;  // _8
+            iterator(T* head, T* tail);
+
+            void operator++();
+
+            T* mHead;  // 0x0
+            T* mTail;  // 0x4
+            T* mEnd;   // 0x8
         };
+
+        inline FixedRingBuffer() {}
+
+        FixedRingBuffer(T* pBuffer, T* pBuffer2) : mHead(pBuffer, pBuffer2), mEnd(pBuffer, pBuffer2), mCount(0) {}
+
+        void push_back(const T& val);
+
+        T mBuffer[S];    // 0x00
+        iterator mHead;  // 0x40 for S=16
+        iterator mEnd;   // 0x4C for S=16
+        s32 mCount;      // 0x58 for S=16
     };
-};
+};  // namespace MR
