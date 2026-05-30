0e0808f3b3b74d4897ddb19638a57c17c3d180e5
Super Hackio <superhaxio@gmail.com>
2026-05-30 13:13:38 -0700

    Binder booleans

--- include/Game/LiveActor/Binder.h
+++ include/Game/LiveActor/Binder.h
@@ -39,5 +39,13 @@ public:
     HitInfo _3C;
     HitInfo _CC;
     HitInfo _15C;
-    u8 _1EC;
+
+    struct {
+        bool _0 : 1;
+        bool _1 : 1;
+        bool _2 : 1;
+        bool _3 : 1;
+        bool _4 : 1;
+        bool _5 : 1;
+    } _1EC;
 };
