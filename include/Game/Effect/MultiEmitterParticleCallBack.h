594800959045d4dbdad6f2dc6c015b2ad17cb0b6
Super Hackio <superhaxio@gmail.com>
2026-05-30 13:13:59 -0700

    Add SpinPullParticleCallback

---
+++ include/Game/Effect/MultiEmitterParticleCallBack.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "JSystem/JParticle/JPAParticleCallBack.h"
+
+class MultiEmitterParticleCallBack : public JPAParticleCallBack {
+public:
+    MultiEmitterParticleCallBack();
+
+    virtual ~MultiEmitterParticleCallBack();
+};
