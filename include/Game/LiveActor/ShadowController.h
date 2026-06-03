45022d081de8ab9c2bcf601e550e07e29857cd15
Super Hackio <superhaxio@gmail.com>
2026-06-03 08:56:28 -0700

    ShadowControllerList

--- include/Game/LiveActor/ShadowController.h
+++ include/Game/LiveActor/ShadowController.h
@@ -2,14 +2,27 @@
 
 #include "revolution.h"
 #include "Game/LiveActor/LiveActor.h"
+#include "Game/Util/Array.h"
+
+class ShadowController;
 
 class ShadowControllerList {
 public:
     ShadowControllerList(LiveActor *, u32);
+    
+    void addController(ShadowController*);
+    u32 getControllerCount() const;
+    ShadowController* getController(u32) const;
+    ShadowController* getController(const char*) const;
+    void resetCalcCount();
+    void requestCalc();
 
-    u8 _0[0x10];
+    MR::Vector< MR::AssignableArray< ShadowController* > > mShadowList;  // 0x0
+    LiveActor* mHost;                                                    // 0xC
 };
 
 namespace MR {
-    void requestCalcActorShadow(LiveActor *);
+    void addShadowController(ShadowController*);
+    void requestCalcActorShadowAppear(LiveActor*);
+    void requestCalcActorShadow(LiveActor*);
 };
