3019972cb9d0242943d9d3b207b11a4f6c90590f
VTXG <VTXG@users.noreply.github.com>
2026-06-25 00:46:23 +0100

    `GameDataPlayerStatus`

---
+++ include/Game/System/GameDataPlayerStatus.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Game/System/BinaryDataChunkHolder.h"
+#include "Game/System/BinaryDataContentHeaderSerializer.h"
+
+class GameDataPlayerStatus : public BinaryDataChunkBase {
+public:
+    GameDataPlayerStatus();
+
+	virtual u32 makeHeaderHashCode() const;
+	virtual u32 getSignature() const;
+	virtual u32 serialize(u8 *, u32) const;
+	virtual u32 deserialize(const u8 *, u32);
+	virtual void initializeData();
+
+    s32 getPlayerLeft() const;
+    s32 addPlayerLeft(s32);
+    s32 getStockedStarPieceNum() const;
+    s32 addStockedStarPieceNum(s32);
+    s32 getStockedCoinNum() const;
+    s32 addStockedCoinNum(s32);
+
+    /* 0x04 */ BinaryDataContentHeaderSerializer* mHeaderSerializer;
+    /* 0x08 */ u8 mPlayerLeft;
+    /* 0x0A */ u16 mStockedStarPieceNum;
+    /* 0x0C */ u16 mStockedCoinNum;
+    /* 0x0E */ u16 mLast1upCoinNum;
+    /* 0x10 */ u8 mFlag;
+    /* 0x14 */ u32 _14;
+}
